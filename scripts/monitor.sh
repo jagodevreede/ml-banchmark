550e72c5de7ffe819970ad9f07b90348fab6d3d0
Jago de Vreede <jagodevreede@MacBook-Pro.local>
2022-07-02 18:01:39 +0200

    Update monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #apt-get install sysstat -y
 #pip3 install gpustat
-echo "time  CPU    %usr   %nice    %sys %iowait    %irq   %soft  %steal  %guest  %gnice   %idle  gpu_usage gpu_mem mem total used free shared  buff/cache   available" > usage.log
+echo "time  CPU  gpu_usage gpu_mem mem total used free shared  buff/cache   available" > usage.log
 while true
 do
-  GPU=$(gpustat --no-color | tail -n 1 | awk -F '[|,]' '{ print $3 $4 }')
-  CPU=$(mpstat | tail -n 1)
-  MEM=$(free | tail -n 2 | head -n 1)
-  echo $CPU $GPU $MEM >> usage.log
+  TIME=$(date +%T)
+  CPU=$(ps -e -o pcpu,pmem,args --sort=pcpu | grep java | tail -n 1 | cut -d" " -f1-4)
+  GPU=$(gpustat --no-color | tail -n 1 | awk -F '[|,/]' '{ print $3 $4 }' | sed 's/%//g')
+  MEM=$(free | tail -n 2 | head -n 1 | awk -F '[:]' '{ print $2 }')
+  echo "$TIME $CPU $GPU $MEM" | tr -s ' ' >> usage.log
   sleep 1
 done
