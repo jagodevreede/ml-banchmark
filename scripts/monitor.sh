58205c69c59f10cc7bba92e5fe92d50372d6a55d
Jago de Vreede <jagodevreede@MacBook-Pro.local>
2022-07-03 10:50:14 +0200

    Fix main, update monitor

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -7,7 +7,7 @@ do
   TIME=$(date +%T)
   CPU=$(ps -e -o pcpu,pmem,args --sort=pcpu | grep java | tail -n 1 | tr -s ' ' | cut -d" " -f1-3)
   GPU=$(gpustat --no-color | tail -n 1 | awk -F '[|,/]' '{ print $3 $4 }' | sed 's/%//g')
-  MEM=$(free | tail -n 2 | head -n 1 | awk -F '[:]' '{ print $2 }')
+  MEM=$(free --mega | tail -n 2 | head -n 1 | awk -F '[:]' '{ print $2 }')
   echo "$TIME $CPU $GPU $MEM" | tr -s ' ' >> usage.csv
   sleep 1
 done
